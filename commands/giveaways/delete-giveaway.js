6a80f5f727ac50e50a14fad28faa3a9c33164247
FnrDev <ah2014bh@outlook.com>
2021-11-10 09:11:04 +0300

    delete giveaway command

---
+++ commands/giveaways/delete-giveaway.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "delete",
+    description: "Delete a giveaway.",
+    options: [
+        {
+            name: "message_id",
+            description: "Message id for giveaway to delete",
+            type: 3,
+            required: true
+        }
+    ],
+    giveawayPerms: true,
+    run: async(interaction, client) => {
+        const giveawayId = interaction.options.getString('message_id');
+        const fetchGiveaway = client.giveawaysManager.giveaways.find(r => r.messageId === giveawayId);
+        if (!fetchGiveaway) {
+            return interaction.reply({ content: `Unable to find giveaway \`${giveawayID}\``, ephemeral: true });
+        }
+        await client.giveawaysManager.delete(giveawayId);
+        interaction.reply({ content: `✅ Successfully deleted giveaway with id \`${giveawayId}\`` })
+    }
+}
