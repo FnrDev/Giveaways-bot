cb2df8aba81e72ad5851b4245b94430e65dc6e32
FnrDev <ah2014bh@outlook.com>
2021-11-10 09:27:42 +0300

    giveaway ended event

---
+++ events/giveaways/giveawayEnded.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+
+module.exports = async(client, giveaway, winners) => {
+    const channel = client.channels.cache.get(process.env.log);
+    if (!channel) return;
+    let winnersText = '';
+    winners.forEach(member => {
+        winnersText += `<@${member.user.id}> `
+    })
+    const embed = new Discord.MessageEmbed()
+    .setTitle('Giveaway Ended')
+    .setColor('#00FFC1')
+    .setTimestamp()
+    .setFooter(giveaway.messageId)
+    .addFields(
+        {
+            name: "Hosted By:",
+            value: giveaway.data.hostedBy
+        },
+        {
+            name: "Giveaway Prize:",
+            value: giveaway.data.prize
+        },
+        {
+            name: "Giveaway Started At:",
+            value: `**<t:${giveaway.data.startAt.toString().slice(0, 10)}:R>**`
+        },
+        {
+            name: "Giveaway End At:",
+            value: `**<t:${giveaway.data.endAt.toString().slice(0, 10)}:R>**`
+        },
+        {
+            name: "Giveaway Winner(s)",
+            value: winnersText
+        },
+    )
+    const row = new Discord.MessageActionRow()
+    .addComponents(
+        new Discord.MessageButton()
+        .setStyle('LINK')
+        .setLabel('Giveaway URL')
+        .setURL(giveaway.messageURL)
+    )
+    channel.send({ embeds: [embed], components: [row] })
+}
